181bc572a78954be79892c2c0b6c352fc07e3140
sauraw1914 <diwasesauraw@gmail.com>
2021-10-27 19:46:34 +0530

    https://github.com/sauraw1914/studentstudyportal.git

---
+++ studentstudyportal/dashboard/forms.py
@@ -0,0 +1,67 @@
+from django import forms
+from django.db.models import fields
+from django.forms import widgets
+from django.forms.forms import Form
+from . models import *
+from django.contrib.auth.forms import UserCreationForm
+
+class NotesForm(forms.ModelForm):
+    class Meta:
+        model=Notes
+        fields=['title','description']
+
+class DateInput(forms.DateInput):
+    input_type='date'
+
+class HomeworkForm(forms.ModelForm):
+    class Meta:
+        model=Homework
+        widgets={"due":DateInput()}
+        fields=['subject','title','description','due','is_finished']
+
+
+class DashboardForm(forms.Form):
+    text=forms.CharField(max_length=100,label="Enter Your Search  ")
+
+
+class TodoForm(forms.ModelForm):
+    class Meta:
+        model=Todo
+        fields=['title','is_finished']
+
+class conversionForm(forms.Form):
+    CHOICE=[('lenght','Lenght'),('mass','Mass')]
+    measurement=forms.ChoiceField(choices=CHOICE,widget=forms.RadioSelect)
+
+
+class conversionLenghtForm(forms.Form):
+    CHOICES=[('yard','Yard'),('foot','Foot')]
+    input=forms.CharField(required=False,label=False,widget=forms.TextInput(
+        attrs={'type':'number','placeholder':'Enter the Number'}
+    ))
+    measure1=forms.CharField(
+        label='',widget=forms.Select(choices=CHOICES)
+    )
+    measure2=forms.CharField(
+        label='',widget=forms.Select(choices=CHOICES)
+    )
+
+
+class conversionMassForm(forms.Form):
+    CHOICES=[('pound','Pound'),('kilogram','Kilogram')]
+    input=forms.CharField(required=False,label=False,widget=forms.TextInput(
+        attrs={'type':'number','placeholder':'Enter the Number'}
+    ))
+    measure1=forms.CharField(
+        label='',widget=forms.Select(choices=CHOICES)
+    )
+    measure2=forms.CharField(
+        label='',widget=forms.Select(choices=CHOICES)
+    )
+    
+
+class UserRegistrationForm(UserCreationForm):
+    class Meta:
+        model=User
+        fields=['username','password1','password2']
+
